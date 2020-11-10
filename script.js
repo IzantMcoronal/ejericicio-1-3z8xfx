c2c031f14264a3d2f353b44d35209842dd5650b2
IzantMcoronal <izant2020@gmail.com>
2020-11-10 12:38:55 +0100

    1 file modified

--- script.js
+++ script.js
@@ -18,11 +18,58 @@
 
 window.addEventListener('load',onLoad);
 
+var users
+
 function onLoad(){
   console.log('hi')
+
+  createUsers();
+  console.log(createUsers)
 }
 
 function createUsers (){
 
-  
+  var persona1 = new Object();
+ persona1.nombre = '';
+ persona1.country = '';
+ persona1.money = 0;
+ persona1.premiumAccount = '';
+
+  var persona2 = new Object();
+ persona2.nombre = '';
+ persona2.country = '';
+ persona2.money = 0;
+ persona2.premiumAccount = '';
+
+  var persona3 = new Object();
+ persona3.nombre = '';
+ persona3.country = '';
+ persona3.money = 0;
+ persona3.premiumAccount = '';
+
+  var persona4 = new Object();
+ persona4.nombre = '';
+ persona4.country = '';
+ persona4.money = 0;
+ persona4.premiumAccount = '';
+
+  var persona5 = new Object();
+ persona5.nombre = '';
+ persona5.country = '';
+ persona5.money = 0;
+ persona5.premiumAccount = '';
+
+  var persona6 = new Object();
+ persona6.nombre = '';
+ persona6.country = '';
+ persona6.money = 0;
+ persona6.premiumAccount = '';
+
+  var persona7 = new Object();
+ persona7.nombre = '';
+ persona7.country = '';
+ persona7.money = 0;
+ persona7.premiumAccount = '';
+
+ Users = [persona1, persona2, persona1, persona3, persona4, persona5, persona6, persona7]
 }
